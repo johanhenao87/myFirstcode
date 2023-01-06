cce9c8017e76d777ffce6314a9055436bee0da65
Johan Andres Rojas <dj.johan87@gmail.com>
2023-01-06 08:38:25 -0500

    Funcion calcular la edad

--- j1.js
+++ j1.js
@@ -1,11 +1,24 @@
 function saludo(nombre) {
   if (nombre == "") {
     return "debes proporcionar tu nombre";
- }else {
-    return "hola " + nombre + " Bienvenido a tu primer codigo";
-  }
+  }else {
+    	return "Hola " + nombre + " Bienvenido a tu primer codigo";
+    	}
 }
 
 var nombre = prompt("ingresa tu nombre");
 
+function aNacimiento(year){
+	if(year == null){
+		return "No has proporcionado tu año de nacimiento";
+	}else{
+		var resultado = (2023-year)
+		return "Tu edad actual es " + resultado +" años";
+	}
+}
+
+var year = prompt("Ingresa tu año de nacimiento ");
+year = parseInt(year); 
+
 document.getElementById("saludando").innerHTML = saludo(nombre);
+document.getElementById("edad").innerHTML = aNacimiento(year);
